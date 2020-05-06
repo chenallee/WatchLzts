f6dff2b0c6c7fa6da0219f19a0db4317844df35e
chenallee <lchenalloy@gmail.com>
2020-05-05 21:18:25 -0400

    setting up user flow

---
+++ client/src/pages/Explore.js
@@ -0,0 +1,13 @@
+import React, { useContext, useEffect } from 'react';
+
+function Explore(){
+
+
+    return(
+        <>
+            <h1>explore page :3</h1>
+        </>
+    );
+}
+
+export default Explore;
---
+++ client/src/pages/Watchlists.js
@@ -0,0 +1,13 @@
+import React, { useContext, useEffect } from 'react';
+
+function Watchlists(){
+
+
+    return(
+        <>
+            <h1>Watchlzts 📺 </h1>
+        </>
+    );
+}
+
+export default Watchlists;
